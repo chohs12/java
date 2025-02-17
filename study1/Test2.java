38b85c0345c5ee1424c338fb29480249fc2714b1
chohs12 <chohs12@daum.net>
2025-02-17 11:42:23 +0900

    Add files via upload

---
+++ study1/Test2.java
@@ -0,0 +1,17 @@
+package study1;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("@ 수강과목 @");
+		System.out.println("<font color='red'>1.java</font>");
+		System.out.println("2.oracle/mysql");
+		System.out.println("3.jsp");
+		System.out.println("4.jsp&서블릿");
+		System.out.println("5.javascript/jquery");
+		System.out.println("6.spring boot");
+		System.out.println("7.linux");
+		System.out.println("8.React");
+	}
+}
